fe45b1ccc91928bd0c6da2314032ab5f6f2c6d71
UnFuzed <Unfuzedyt@gmail.com>
2026-03-01 17:13:36 -0700

    updated miku card

--- script/c39393939.lua
+++ script/c39393939.lua
@@ -1,8 +1,8 @@
 -- Vocaloid – Hatsune Miku
 
--- Only for VSCode autocomplete
 ---@diagnostic disable: undefined-global
 
+-- Only for VSCode autocomplete
 if false then
     require("edo_const.constant")
     require("edo_const.card_counter_constants")
@@ -15,40 +15,117 @@ if false then
 end
 
 local s,id=GetID()
+
 function s.initial_effect(c)
-    -- Add 1 "Vocaloid" card from Deck to hand when Summoned
+    -------------------------------------------------
+    -- Effect 1: Search when Normal/Special Summoned
+    -------------------------------------------------
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))      -- Effect description
+    e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)          -- Normal Summon
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCountLimit(1,id)                     -- Once per turn
+    e1:SetCountLimit(1,id)
     e1:SetTarget(s.thtg)
     e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
+
     local e2=e1:Clone()
-    e2:SetCode(EVENT_SPSUMMON_SUCCESS)        -- Special Summon
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e2)
+
+    -------------------------------------------------
+    -- Effect 2: Quick Effect – Discard to Special Summon
+    -------------------------------------------------
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+    e3:SetCountLimit(1,id+100)
+    e3:SetCost(s.spcost)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
 end
 
--- Filter for "Vocaloid" cards
+-------------------------------------------------
+-- SEARCH EFFECT
+-------------------------------------------------
+
 function s.thfilter(c)
     return c:IsSetCard(0x3939) and c:IsAbleToHand()
 end
 
--- Target function
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+    end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
--- Operation function
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-------------------------------------------------
+-- QUICK SPECIAL SUMMON EFFECT
+-------------------------------------------------
+
+-- Cost: Discard 1 card + apply Vocaloid-only lock
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+    end
+
+    -- Discard 1 card
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+
+    -- Apply Special Summon restriction
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(s.splimit)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+end
+
+-- Lock: Cannot Special Summon non-Vocaloid monsters
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+    return not c:IsSetCard(0x3939)
+end
+
+-- Filter for Special Summon
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0x3939)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+-- Target
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+
+-- Operation
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end
