84bd977de8730082f74d0f461a57f7c213c14370
UnFuzed <Unfuzedyt@gmail.com>
2026-03-26 19:27:27 -0600

    idk

--- script/c39393939.lua
+++ script/c39393939.lua
@@ -15,32 +15,28 @@ function s.initial_effect(c)
     e0:SetCondition(s.handcon)
     c:RegisterEffect(e0)
 
-    --Activate in set turn (IMPORTANT)
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-    c:RegisterEffect(e2)
-
     --Activate
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_DRAW)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_CHAINING)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
     e1:SetCondition(s.condition)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
 
+-- Always allow from hand
 function s.handcon(e)
     return true
 end
 
+-- Check if chain can be negated
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
     return Duel.IsChainNegatable(ev)
 end
 
+-- Target
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(tp,4) end
     Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
@@ -48,6 +44,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,4)
 end
 
+-- Effect
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local rc=re:GetHandler()
     if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
