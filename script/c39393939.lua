4a7c6d59054ac79ba31e3b78e33ae72ac7524024
UnFuzed <Unfuzedyt@gmail.com>
2026-03-26 19:37:50 -0600

    idk

--- script/c39393939.lua
+++ script/c39393939.lua
@@ -1,7 +1,3 @@
---Ultimate Void Trap
-
----@diagnostic disable: undefined-global
-
 local s,id=GetID()
 function s.initial_effect(c)
     --Activate from hand
@@ -11,38 +7,51 @@ function s.initial_effect(c)
     e0:SetCondition(s.handcon)
     c:RegisterEffect(e0)
 
-    --Activate
+    --Negate effects
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_DRAW)
+    e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DRAW)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
-    e1:SetCondition(s.condition)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
 
+--Hand trap always activatable
 function s.handcon(e)
     return true
 end
 
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    -- Only allow activation if currently chaining something negatable
-    return Duel.GetCurrentChain()>0 and Duel.IsChainNegatable(Duel.GetCurrentChain())
+--Target one face-up monster or Spell/Trap
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsFaceup() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,4) end
-    Duel.SetOperationInfo(0,CATEGORY_NEGATE,nil,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,4)
-end
-
+--Negate the effects
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local chain=Duel.GetCurrentChain()
-    if chain>0 then
-        Duel.NegateEffect(chain)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetValue(RESET_TURN_SET)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e2)
     end
-    Duel.Draw(tp,4,REASON_EFFECT)
+    --Draw 2 cards
+    Duel.Draw(tp,2,REASON_EFFECT)
 end
